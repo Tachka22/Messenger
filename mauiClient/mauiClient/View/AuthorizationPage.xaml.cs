7c01394e0ff2c86e005f74769d3668310202569a
Vlad <chinnov.vlad@gmail.com>
2024-02-01 12:25:29 +0300

    навигация(тестоваяя)

--- mauiClient/mauiClient/View/AuthorizationPage.xaml.cs
+++ mauiClient/mauiClient/View/AuthorizationPage.xaml.cs
@@ -1,3 +1,5 @@
+
+
 namespace mauiClient.View;
 
 public partial class AuthorizationPage : ContentPage
@@ -5,5 +7,18 @@ public partial class AuthorizationPage : ContentPage
 	public AuthorizationPage()
 	{
 		InitializeComponent();
+        btn_Registrations.Clicked += Btn_Registrations_Clicked;
+        btn_Authorization.Clicked += Btn_Authorization_Clicked;
+
 	}
+
+    private async void Btn_Authorization_Clicked(object? sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
+
+    private async  void Btn_Registrations_Clicked(object? sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new RegistrationPage());
+    }
 }
