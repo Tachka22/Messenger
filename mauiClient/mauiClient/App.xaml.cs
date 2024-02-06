f9fc65af1c85a4f86d14268aa3e8d7e47d53bd6c
Vlad <chinnov.vlad@gmail.com>
2024-02-06 23:02:05 +0300

    Переопределил стартовые размеры приложения

--- mauiClient/mauiClient/App.xaml.cs
+++ mauiClient/mauiClient/App.xaml.cs
@@ -10,5 +10,14 @@ namespace mauiClient
 
             MainPage = new NavigationPage(new AuthorizationPage());
         }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            const int newWidth = 1200;
+            const int newHeight = 850;
+            window.Width = newWidth;
+            window.Height = newHeight;
+            return window;
+        }
     }
 }
