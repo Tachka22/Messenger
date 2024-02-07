7214a3dec7ecd2acad8b239328033f73fcbf67f6
Vlad <chinnov.vlad@gmail.com>
2024-02-08 00:52:30 +0300

    Зарегал маршруты страниц

--- mauiClient/mauiClient/AppShell.xaml.cs
+++ mauiClient/mauiClient/AppShell.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace mauiClient
+﻿using mauiClient.View;
+
+namespace mauiClient
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(LoadingPage), typeof(LoadingPage));
+            Routing.RegisterRoute(nameof(AuthorizationPage), typeof(AuthorizationPage));
+            Routing.RegisterRoute(nameof(HomeChatsPage), typeof(HomeChatsPage));
         }
     }
 }
