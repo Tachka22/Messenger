c63cf22673edbe85f0f4d9e47fb749c27bfee904
Vlad <chinnov.vlad@gmail.com>
2024-02-08 21:11:07 +0300

    Вывод списка чатов

--- mauiClient/mauiClient/View/HomeChatsPage.xaml.cs
+++ mauiClient/mauiClient/View/HomeChatsPage.xaml.cs
@@ -13,9 +13,9 @@ public partial class HomeChatsPage : ContentPage
 	public HomeChatsPage()
 	{
 		InitializeComponent();
-        ListView listView = new ListView();
-        listView.Margin = 10;
-
+        ListView listView = new ListView { };
+        listView.SeparatorVisibility = SeparatorVisibility.Default;
+        listView.SeparatorColor = Color.FromArgb("#eaeaea");
         // определяем источник данных
         listView.ItemsSource = new List<Language>
         {
@@ -31,22 +31,24 @@ public partial class HomeChatsPage : ContentPage
             new Language {Name="Iban", ImagePath="chatavatar.png", Description = "How?" },
             new Language {Name="Ann", ImagePath="chatavatar.png", Description = "I want five" },
             new Language {Name="Vova", ImagePath="chatavatar.png", Description = "are you?" },
+            new Language {Name="Ann", ImagePath="chatavatar.png", Description = "I want five" },
+            new Language {Name="Vova", ImagePath="chatavatar.png", Description = "are you?" },
+            new Language {Name="Vlad", ImagePath="chatavatar.png", Description = "Hi!" },
+            new Language {Name="Iban", ImagePath="chatavatar.png", Description = "How?" },
+            new Language {Name="Ann", ImagePath="chatavatar.png", Description = "I want five" },
+            new Language {Name="Vova", ImagePath="chatavatar.png", Description = "are you?" },
         };
         // определяем шаблон данных
         listView.ItemTemplate = new DataTemplate(() =>
         {
-            ImageCell imageCell = new ImageCell
-            {
-                TextColor = Color.FromArgb("#2E2B2B"),
-                DetailColor = Color.FromArgb("#90A4AE"),
-            };
-            imageCell.SetBinding(ImageCell.TextProperty, "Name");
-            imageCell.SetBinding(ImageCell.DetailProperty, "Description");
-            imageCell.SetBinding(ImageCell.ImageSourceProperty, "ImagePath");
-            return imageCell;
+            CustomCell customCell = new CustomCell { ImageHeight = 50, ImageWidth = 50 };
+
+            customCell.SetBinding(CustomCell.TitleProperty, "Name");
+            customCell.SetBinding(CustomCell.DetailProperty, "Description");
+            customCell.SetBinding(CustomCell.ImagePathProperty, "ImagePath");
+            return customCell;
         });
         
-        Label header = new Label { FontSize = 18, Text = "Языки программирования" };
         StackL.Add(new Microsoft.Maui.Controls.StackLayout { Children = { listView }, Padding = 7, });
     }
 }
