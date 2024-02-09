2ce206d0bc76eb283294b9332cac119e7432e23d
Vlad <chinnov.vlad@gmail.com>
2024-02-10 01:08:34 +0300

    Реализвал ViewModel: команду выхода и перехода на страницу настройки личных данных

--- mauiClient/mauiClient/ViewModel/SettingViewModel.cs
+++ mauiClient/mauiClient/ViewModel/SettingViewModel.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using mauiClient.Model;
+using mauiClient.Services;
 
 namespace mauiClient.ViewModel
 {
-    class SettingViewModel
+    public partial class SettingViewModel:ObservableObject
     {
+        [ObservableProperty]
+        private User user;
+
+        private readonly ClientService _settingService;
+        public SettingViewModel()
+        {
+            User = new();
+        }
+
+        [RelayCommand]
+        private async Task LogOut()
+        {
+            bool answer = await Shell.Current.DisplayAlert("Attention!", "Do you want to log out of your account?", "Yes", "No");
+            if (answer)
+            {
+                // TODO: Написать выход из аккаунта.
+            }
+        }
+        [RelayCommand]
+        private async Task GoToChangeProfilePage()
+        {
+            await Shell.Current.GoToAsync($"//{nameof(View.SettingPages.ChangeProfilePage)}");
+        }
     }
 }
