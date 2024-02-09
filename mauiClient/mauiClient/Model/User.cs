68cc7c8e5d6e45684110385e4dcc75070fdf41df
Vlad <chinnov.vlad@gmail.com>
2024-02-10 01:04:48 +0300

    Добавил свойства

--- mauiClient/mauiClient/Model/User.cs
+++ mauiClient/mauiClient/Model/User.cs
@@ -8,12 +8,14 @@ namespace mauiClient.Model
 {
     public class User
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Email { get; set; }
-        public string UserName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
+        public Guid? Id { get; set; }
+        public string? Name { get; set; } = "Vlad";
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? UserName { get; set; } = "User294738";
+        public string? PhoneNumber { get; set; } = "7(999)-999-99-99";
+        public string? Password { get; set; }
+        public string? Bio { get; set; }
+        public string? PhotoSource { get; set; }
     }
 }
