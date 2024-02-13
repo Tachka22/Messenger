4d87237a4972edcac17cf8ec0f673f3d963ae749
Vlad <chinnov.vlad@gmail.com>
2024-02-13 22:55:49 +0300

    Изменил почту на номер телефона

--- mauiClient/mauiClient/Model/LoginParams.cs
+++ mauiClient/mauiClient/Model/LoginParams.cs
@@ -8,7 +8,7 @@ namespace mauiClient.Model
 {
     public class LoginParams
     {
-        public string? Email {get; set;}
+        public string? PhoneNumber { get; set;}
         public string? Password { get; set;}
     }
 }
