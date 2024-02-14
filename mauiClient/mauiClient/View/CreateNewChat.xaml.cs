e0133246aab4aa93cfe7c43fbcf993c55f3337bd
Vlad <chinnov.vlad@gmail.com>
2024-02-14 23:59:21 +0300

    Создание нового чата

--- mauiClient/mauiClient/View/CreateNewChat.xaml.cs
+++ mauiClient/mauiClient/View/CreateNewChat.xaml.cs
@@ -1,9 +1,12 @@
+using mauiClient.ViewModel;
+
 namespace mauiClient.View;
 
 public partial class CreateNewChat : ContentPage
 {
-	public CreateNewChat()
+	public CreateNewChat(ChatViewModel chatViewModel)
 	{
 		InitializeComponent();
+		BindingContext = chatViewModel;
 	}
 }
